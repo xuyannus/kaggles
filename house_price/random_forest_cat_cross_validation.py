031c9f2197802e8f3b9a3b3afab6bfea1e7e2524
Yan <xuyan.nus@gmail.com>
2020-12-27 19:29:13 +1100

    random forest

--- house_price/random_forest_cat_cross_validation.py
+++ house_price/random_forest_cat_cross_validation.py
@@ -17,14 +17,9 @@ ALL_FEATURES = ['GrLivArea',
                 'MSSubClass',
                 'TotalBsmtSF',
                 'LotArea',
-                'BsmtFinSF1',
-                '1stFlrSF',
-                'GarageArea',
-                'OpenPorchSF',
                 'Neighborhood',
                 'YearBuilt',
                 'YearRemodAdd',
-                '2ndFlrSF',
                 'OverallQual',
                 'OverallCond',
                 'WoodDeckSF',
@@ -34,22 +29,26 @@ ALL_FEATURES = ['GrLivArea',
                 'GarageCars',
                 'BedroomAbvGr',
                 'ExterQual',
+                'BsmtQual',
                 'Exterior1st',
+                'KitchenQual',
+                'FireplaceQu',
                 'TotRmsAbvGrd',
                 'FullBath',
                 'HalfBath',
                 'BsmtFullBath',
-                'BsmtHalfBath',
-                'EnclosedPorch']
+                'BsmtHalfBath']
 
 CAT_FEATURES = ['Neighborhood',
                 'MSSubClass',
                 'GarageQual',
                 'ExterQual',
+                'BsmtQual',
+                'KitchenQual',
+                'FireplaceQu',
                 'Exterior1st',
                 'MoSold',
-                'YrSold',
-                ]
+                'YrSold']
 
 
 def load_data():
@@ -92,19 +91,22 @@ def fill_missing_bsmt_fin_1(x, df):
 
 
 def missing_data_filling(df):
-    default_garage_area = df.groupby('YearBuilt')['GarageArea'].median().to_dict()
-    df['GarageArea'] = df.apply(lambda x: fill_missing_garage_area(x, default_garage_area), axis=1)
+    # default_garage_area = df.groupby('YearBuilt')['GarageArea'].median().to_dict()
+    # df['GarageArea'] = df.apply(lambda x: fill_missing_garage_area(x, default_garage_area), axis=1)
 
     default_garage_cars = df.groupby('YearBuilt')['GarageCars'].median().to_dict()
     df['GarageCars'] = df.apply(lambda x: fill_missing_garage_cars(x, default_garage_cars), axis=1)
 
     df['GarageQual'].fillna('NoGarage', inplace=True)
+    df['BsmtQual'].fillna('NoBasement', inplace=True)
+    df['FireplaceQu'].fillna('NoFireplace', inplace=True)
     # df['GarageFinish'].fillna('NoGarage', inplace=True)
     # df['GarageYrBlt'].fillna(df['YearBuilt'], inplace=True)
     df['TotalBsmtSF'] = df.apply(lambda x: fill_missing_total_bsmt(x, df), axis=1)
-    df['BsmtFinSF1'] = df.apply(lambda x: fill_missing_bsmt_fin_1(x, df), axis=1)
+    # df['BsmtFinSF1'] = df.apply(lambda x: fill_missing_bsmt_fin_1(x, df), axis=1)
 
     explore_none(df)
+
     return df
 
 
@@ -122,6 +124,13 @@ def house_new(x):
         return 0
 
 
+def bin_exterior1st(x):
+    if x not in ['VinylSd', 'HdBoard', 'MetalSd', 'Wd Sdng', 'Plywood']:
+        return 'Others'
+    else:
+        return x
+
+
 def bin_negihbour(x):
     if x in ['StoneBr', 'NridgHt', 'NoRidge']:
         return 'HighPriceSuburb'
@@ -131,6 +140,21 @@ def bin_negihbour(x):
         return 'MediumPriceSuburb'
 
 
+def bin_total_sq_feet(x):
+    if x <= 1000:
+        return 1
+    elif x<= 2000:
+        return 2
+    elif x <= 2500:
+        return 3
+    elif x <= 3000:
+        return 4
+    elif x <= 4000:
+        return 5
+    else:
+        return 6
+
+
 def extract_common_features(df):
     df.loc[:, 'TotBathrooms'] = df['FullBath'] + df['HalfBath'] * 0.5 + df['BsmtFullBath'] + df['BsmtHalfBath'] * 0.5
     df.drop(['FullBath', 'HalfBath', 'BsmtFullBath', 'BsmtHalfBath'], axis=1, inplace=True)
@@ -138,15 +162,25 @@ def extract_common_features(df):
     df.loc[:, 'HouseAge'] = (df['YrSold'] - df['YearRemodAdd']).clip(0, 1000)
     df.loc[:, 'HouseRemod'] = df.apply(lambda x: house_renovated(x), axis=1)
     df.loc[:, 'HouseNew'] = df.apply(lambda x: house_new(x), axis=1)
+    df.drop(['YearBuilt', 'YearRemodAdd'], axis=1, inplace=True)
+
     df.loc[:, 'Neighborhood'] = df['Neighborhood'].apply(lambda x: bin_negihbour(x))
+    df.loc[:, 'MoSold'] = df['MoSold'].apply(lambda x: int((x - 1) / 3))
+
+    df.loc[:, 'TotalSqFeet'] = df['GrLivArea'] + df['TotalBsmtSF']
+    df.loc[:, 'TotalSqFeet'] = df['TotalSqFeet'].apply(lambda x: bin_total_sq_feet(x))
+    df.drop(['TotalBsmtSF'], axis=1, inplace=True)
+
+    df.loc[:, 'Exterior1st'] = df['Exterior1st'].apply(lambda x: bin_exterior1st(x))
 
     return df
 
 
-def drop_outliers(df):
+def drop_outliers(df, y):
     # those two houses are huge and high quality but low price
     df.drop([524, 1299], inplace=True)
-    return df
+    y = np.delete(y, [524, 1299])
+    return df, y
 
 
 def one_hot_encoding(df):
@@ -163,15 +197,17 @@ def cal_accuracy(y_actual, y_predicted):
     return mean_squared_error(y_actual, y_predicted, squared=False)
 
 
-def cross_validate(features_df, y, params, k=3):
+def print_top_k_features(model, features):
+    print("---------")
+    for x, y in sorted(zip(model.feature_importances_, features), reverse=True)[:20]:
+        print({"feature": y, "score": x})
+    print("---------")
+
+
+def cross_validate(features_df, y, params, k=5):
     kf3 = KFold(n_splits=k, shuffle=True)
     train_accuracy_list = []
     test_accuracy_list = []
-
-    some_features = ['OverallQual', 'GrLivArea', 'GarageCars', 'GarageArea', 'TotalBsmtSF', '1stFlrSF',
-                     'TotRmsAbvGrd', 'YearRemodAdd', 'BsmtFinSF1', 'WoodDeckSF', '2ndFlrSF',
-                     'OpenPorchSF', 'LotArea']
-    features_df = features_df[some_features]
 
     for tune_train_index, tune_test_index in kf3.split(features_df):
         X_train = features_df.iloc[tune_train_index].copy()
@@ -182,8 +218,10 @@ def cross_validate(features_df, y, params, k=3):
         rf_model = RandomForestRegressor(random_state=31,
                                          max_depth=params['max_depth'],
                                          n_estimators=params['n_estimators'],
+                                         min_samples_leaf=params['min_samples_leaf'],
                                          max_features=params['max_features'])
         rf_model.fit(X_train, y_train)
+        # print_top_k_features(rf_model, X_train.columns)
 
         y_train_hat = rf_model.predict(X_train)
         y_test_hat = rf_model.predict(X_test)
@@ -201,23 +239,26 @@ def cross_validate(features_df, y, params, k=3):
 
 def hyper_params_tuning():
     train_df = load_data()
+    y = train_df['SalePrice'].values
 
     features_df = selected_features(train_df)
     features_df = missing_data_filling(features_df)
     features_df = extract_common_features(features_df)
-    features_df = drop_outliers(features_df)
+
+    features_df, y = drop_outliers(features_df, y)
     features_df = one_hot_encoding(features_df)
 
     param_grid = {
-        'max_depth': [5, 6, 7, 8, 9, 10, 11, 12],
-        'max_features': ['sqrt', None],
-        'n_estimators': [20, 50, 100, 500, 1000]
+        'max_depth': [5, 10, 50],
+        'max_features': [None],
+        'min_samples_leaf': [4, 8, 16, 32],
+        'n_estimators': [20, 50, 100, 500]
     }
 
     optimal_params = None
     optimal_accuracy = 100.0
     for params in ParameterGrid(param_grid):
-        result = cross_validate(features_df, train_df['SalePrice'].values, params)
+        result = cross_validate(features_df, y, params)
         print("---------------")
         print(params)
         print(result)
@@ -231,6 +272,6 @@ def hyper_params_tuning():
     })
 
 
-# {'optimal_params': {'max_depth': 9, 'max_features': None, 'n_estimators': 500}, 'optimal_accuracy': 0.1462004605199809}
+# {'max_depth': 10, 'max_features': None, 'min_samples_leaf': 4, 'n_estimators': 100}, 'optimal_accuracy': 0.14305061923002066}
 if __name__ == "__main__":
     hyper_params_tuning()
