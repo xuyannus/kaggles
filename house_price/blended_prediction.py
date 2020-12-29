d312d716450342d168b135eb9d99014c560e5fc7
Yan <xuyan.nus@gmail.com>
2020-12-29 13:11:31 +1100

    all submissions

---
+++ house_price/blended_prediction.py
@@ -0,0 +1,13 @@
+import os
+import pandas as pd
+import numpy as np
+
+lasso = pd.read_csv(os.path.dirname(__file__) + "/submission_lasso_v1.csv")
+lgbm = pd.read_csv(os.path.dirname(__file__) + "/submission_lgbm_v1.csv")
+rf = pd.read_csv(os.path.dirname(__file__) + "/submission_rf_v1.csv")
+xgb = pd.read_csv(os.path.dirname(__file__) + "/submission_xgb_v1.csv")
+
+combined_df = pd.concat([lasso, lgbm, rf, xgb])
+pred = combined_df.groupby("id")['SalePrice'].median().to_frame()
+pred["id"] = pred.index
+pred[["id", "SalePrice"]].to_csv("./house_price/submission_blended_v1.csv", index=False)
